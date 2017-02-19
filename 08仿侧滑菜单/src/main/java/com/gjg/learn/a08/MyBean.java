6da867734f72a9a0f180a3701c77aa29aa2295b1
GaoJunGuang <1024638043@qq.com>
2017-02-19 18:59:19 +0800

    添加效果图

---
+++ 08仿侧滑菜单/src/main/java/com/gjg/learn/a08/MyBean.java
@@ -0,0 +1,23 @@
+package com.gjg.learn.a08;
+
+/**
+ * Created by JunGuang_Gao
+ * on 2017/2/19  18:30.
+ * 类描述：
+ */
+
+public class MyBean {
+    private String name;
+
+    public MyBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
